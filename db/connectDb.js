e26c18bccb91b8fea2504b2990b9bbfccc3319f6
Nityom Tikhe <nityomtikherr@gmail.com>
2024-07-23 22:57:42 +0530

    db changed

--- db/connectDb.js
+++ db/connectDb.js
@@ -1,8 +1,9 @@
 import mongoose from 'mongoose';
-
+// vHI8ygxURmRAvYGq
+// nityomtikherr
 const connectDB = async () => {
     try {
-      const conn = await mongoose.connect(`${process.env.MONGODB_URI}/getMeAChai`);
+      const conn = await mongoose.connect("mongodb+srv://nityomtikherr:vHI8ygxURmRAvYGq@cluster0.h0exmbi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
       console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
       console.error(error.message);
